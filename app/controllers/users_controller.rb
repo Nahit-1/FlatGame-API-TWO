4462029231e90336f8e0419a2520cbc8c0311d94
Nahit-1 <nahit86@outlook.com>
2019-06-17 21:51:02 +0100

    small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    def index 
+        @user = User.all
+        render json: User.all
+    end 
+
+    def show 
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user
+        else 
+            render json: {error: 'User not found.'}, status: 404 
+        end 
+    end 
 end
