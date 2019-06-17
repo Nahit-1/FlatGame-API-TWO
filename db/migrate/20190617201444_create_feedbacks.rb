e9f3da47f356fad64933e7c151fb5e84fd6ec3c0
Nahit-1 <nahit86@outlook.com>
2019-06-17 21:29:12 +0100

    created db, models, controllers

---
+++ db/migrate/20190617201444_create_feedbacks.rb
@@ -0,0 +1,12 @@
+class CreateFeedbacks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feedbacks do |t|
+      t.text :content
+      t.references :user, foreign_key: true
+      t.references :game, foreign_key: true
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
