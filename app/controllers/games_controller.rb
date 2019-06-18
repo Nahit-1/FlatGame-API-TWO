97d68acb8abd2ede85ae8b7e88e1d4d70033ae3a
Nahit-1 <nahit86@outlook.com>
2019-06-18 11:25:53 +0100

    pre-new-branch

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,4 +12,22 @@ class GamesController < ApplicationController
             render json: {error: 'Game not found.'}, status: 404 
         end 
     end 
+
+    # def rate
+    #     game = Game.find_by(id: params[:game_id])
+    #     if game
+    #         # game.rating += 1
+    #         # game.save 
+    #         game.update(rating: game.rating + 1)
+    #         render json: game
+    #     else
+    #         render json: {error: 'Game not found.'}, status: 404
+    #     end 
+    # end
 end
+
+# fetch('http://localhost:3001/rate', {
+#     method: 'POST',
+#     headers: { 'Content-Type': 'application/json'},
+#     body: JSON.stringify({ game_id: Game.last })
+# })
