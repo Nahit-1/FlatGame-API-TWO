c634a54850dcef965bd7208bf7e3eefe98283413
Nahit-1 <nahit86@outlook.com>
2019-06-18 13:50:43 +0100

    feedback/comments working

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -11,5 +11,31 @@ class FeedbacksController < ApplicationController
         else
           render json: { error: 'Feedback not found.' }, status: 404
         end
+      end
+
+        def create
+          # feedback = Feedback.new =(content: params[:content], user_id: params[:user_id], game_id: params[:game_id], rating: params[:rating])
+          feedback = Feedback.new(content: params[:content], game_id: params[:game_id], rating: params[:rating], user_id: params[:user_id])
+          # byebug
+          if feedback.save
+            render json: feedback
+          else 
+            render json: { error: 'Unable to create feedback.' }, status: 400
+        end 
     end
+
+    # private 
+
+    # def feedback_params
+    #   params.permit([:content, :user_id, :game_id, :rating)
+    # end 
 end
+
+# create_table "feedbacks", force: :cascade do |t|
+#   t.text "content"
+#   t.bigint "user_id"
+#   t.bigint "game_id"
+#   t.integer "rating"
+#   t.index ["game_id"], name: "index_feedbacks_on_game_id"
+#   t.index ["user_id"], name: "index_feedbacks_on_user_id"
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :games
   resources :feedbacks
 
+  
+
   # post /'rate', to: 'game#rating'
 end 
